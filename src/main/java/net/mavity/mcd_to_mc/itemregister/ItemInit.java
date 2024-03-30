a60f89b19eaf8a9c1a55d16c283c0a829b9d1bb2
AngerVon2468 <thomaschad0909@gmail.com>
2024-03-30 15:56:35 +1100

    Anchor & Sword
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/mcd_to_mc/itemregister/ItemInit.java
+++ src/main/java/net/mavity/mcd_to_mc/itemregister/ItemInit.java
@@ -1,24 +1,35 @@
 package net.mavity.mcd_to_mc.itemregister;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.mavity.mcd_to_mc.MCDungeonsToMC;
 import net.mavity.mcd_to_mc.itemregister.itemtypes.BaseSwordType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ItemInit {
+    private static void addToCombatTab(FabricItemGroupEntries entries) {
+        entries.add(ItemInit.SWORD);
+    }
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(MCDungeonsToMC.MOD_ID, name), item);
     }
 
-    public static final Item SWORD = registerItem("sword",
-            new BaseSwordType(ToolMaterials.IRON, 14, -2.4f, new FabricItemSettings().maxDamage(1000)));
-
+    public static final Item SWORD = registerItem("sword_steel",
+            new BaseSwordType(ToolMaterials.IRON, 2, -2.4f, new FabricItemSettings().maxDamage(0)));
+    public static final Item ANCHOR = registerItem("anchor",
+            new SwordItem(ToolMaterials.DIAMOND, 10, -3.4f, new FabricItemSettings().maxDamage(0)));
 
     public static void registerMCDItems() {
-        MCDungeonsToMC.LOGGER.info(MCDungeonsToMC.MOD_ID + " has registered its' items??");
+        MCDungeonsToMC.LOGGER.info(MCDungeonsToMC.MOD_ID + " has registered its' items.");
+
+        //CREATIVE MODE TAB ADDING
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(ItemInit::addToCombatTab);
     }
 }
---
+++ src/main/java/net/mavity/mcd_to_mc/itemregister/itemtypes/BaseAnchorType.java
@@ -0,0 +1,84 @@
+package net.mavity.mcd_to_mc.itemregister.itemtypes;
+
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.Vanishable;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class BaseAnchorType extends ToolItem implements Vanishable {
+    private final float attackDamage;
+    private final Multimap<EntityAttribute, EntityAttributeModifier> attributeModifiers;
+
+    public BaseAnchorType(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
+        super(toolMaterial, settings);
+        this.attackDamage = (float)attackDamage + toolMaterial.getAttackDamage();
+        ImmutableMultimap.Builder<EntityAttribute, EntityAttributeModifier> builder = ImmutableMultimap.builder();
+        builder.put(EntityAttributes.GENERIC_ATTACK_DAMAGE, new EntityAttributeModifier(ATTACK_DAMAGE_MODIFIER_ID, "Weapon modifier", (double)this.attackDamage, EntityAttributeModifier.Operation.ADDITION));
+        builder.put(EntityAttributes.GENERIC_ATTACK_SPEED, new EntityAttributeModifier(ATTACK_SPEED_MODIFIER_ID, "Weapon modifier", (double)attackSpeed, EntityAttributeModifier.Operation.ADDITION));
+        this.attributeModifiers = builder.build();
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public boolean canMine(BlockState state, World world, BlockPos pos, PlayerEntity miner) {
+        return !miner.isCreative();
+    }
+
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if (state.isOf(Blocks.COBWEB)) {
+            return 15.0F;
+        } else {
+            return state.isIn(BlockTags.SWORD_EFFICIENT) ? 1.5F : 1.0F;
+        }
+    }
+
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damage(1, attacker, (e) -> {
+            e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+        });
+        return true;
+    }
+
+    public boolean postMine(ItemStack stack, World world, BlockState state, BlockPos pos, LivingEntity miner) {
+        if (state.getHardness(world, pos) != 0.0F) {
+            stack.damage(2, miner, (e) -> {
+                e.sendEquipmentBreakStatus(EquipmentSlot.MAINHAND);
+            });
+        }
+
+        return true;
+    }
+
+    public boolean isSuitableFor(BlockState state) {
+        return state.isOf(Blocks.COBWEB);
+    }
+
+    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(EquipmentSlot slot) {
+        return slot == EquipmentSlot.MAINHAND ? this.attributeModifiers : super.getAttributeModifiers(slot);
+    }
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext options) {
+        tooltip.add(Text.translatable("tooltip.mcd_to_mc.base_anchor_type"));
+        super.appendTooltip(stack, world, tooltip, options);
+    }
+
+}
