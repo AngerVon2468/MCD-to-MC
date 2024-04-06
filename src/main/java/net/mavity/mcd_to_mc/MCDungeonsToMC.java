b8462669289eed91352cb93d74f34104dea205ff
AngerVon2468 <thomaschad0909@gmail.com>
2024-04-06 16:59:23 +1100

    Fuck you and goodbye blocks
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/mcd_to_mc/MCDungeonsToMC.java
+++ src/main/java/net/mavity/mcd_to_mc/MCDungeonsToMC.java
@@ -4,7 +4,6 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigHolder;
 import net.fabricmc.api.ModInitializer;
 
-import net.mavity.mcd_to_mc.blockregister.BlockInit;
 import net.mavity.mcd_to_mc.commandregister.CommandInit;
 import net.mavity.mcd_to_mc.config.MCDConfig;
 import net.mavity.mcd_to_mc.config.MavConfigSerializer;
@@ -53,7 +52,6 @@ public class MCDungeonsToMC implements ModInitializer {
 		FreezingEnchantment.registerMCDFreezingEnchantment();
 		ThunderingEnchantment.registerMCDThunderingEnchantment();
 		DeathEnchantment.registerMCDFreezingEnchantment();
-		BlockInit.registerMCDBlocks();
 
 		Registry.register(Registries.ENCHANTMENT, new Identifier(MCDungeonsToMC.MOD_ID, "thundering"), THUNDERING);
 		Registry.register(Registries.ENCHANTMENT, new Identifier(MCDungeonsToMC.MOD_ID, "freezing"), FREEZING);
