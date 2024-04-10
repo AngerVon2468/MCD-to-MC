99ce5f3490bf83ec3c78ea616f67f0dfe252f361
AngerVon2468 <thomaschad0909@gmail.com>
2024-04-10 16:46:04 +1000

    I'm just fucking around at this point
    Signed-off-by: AngerVon2468 <thomaschad0909@gmail.com>

--- src/main/java/net/mavity/mcd_to_mc/itemregister/ItemTags.java
+++ src/main/java/net/mavity/mcd_to_mc/itemregister/ItemTags.java
@@ -1,10 +1,11 @@
 package net.mavity.mcd_to_mc.itemregister;
 
+import net.mavity.mcd_to_mc.MCDungeonsToMC;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class ItemTags {
-    public static final TagKey<Item> APPLY_ENCHANT_TAG = TagKey.of(RegistryKeys.ITEM, new Identifier("mcd_to_mc", "applyenchanttag"));
+    public static final TagKey<Item> APPLY_ENCHANT_TAG = TagKey.of(RegistryKeys.ITEM, new Identifier(MCDungeonsToMC.MOD_ID, "applyenchanttag"));
 }
